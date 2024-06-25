1cb9034d2d33b003bfa937f885e1ee71eac0fa94
Yuwei Liang <z5433878@ad.unsw.edu.au>
2024-06-25 14:19:52 +0800

    Modify codes and structures

---
+++ Shared/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize Game
+//
+//  Created by yuweiL on 25/6/2024.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
---
+++ Shared/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize Game
+//
+//  Created by yuweiL on 25/6/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
