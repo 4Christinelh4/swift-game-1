65ca37a1beb5c7caa90afc95b7ed1ec997018ca7
Yuwei Liang <z5433878@ad.unsw.edu.au>
2024-06-26 02:30:36 +0800

    Add Pie shape

---
+++ Shared/Pie.swift
@@ -0,0 +1,18 @@
+//
+//  Pie.swift
+//  Memorize Game (iOS)
+//
+//  Created by yuweiL on 25/6/2024.
+//
+
+import SwiftUI
+
+struct Pie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pie()
+}
